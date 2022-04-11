24ce2f5e0adbb3f8efcdc1c78d4ace46ca9a70e5
Slav10 <sliv2207@colorado.edu>
2022-04-11 12:00:02 -0600

    commit 1

---
+++ 240.Search2DMatrix2/204.main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define M 3
+#define N 4
+
+class Solution{
+    public: 
+        bool searchMatric(vector<vector<int>> & matrix, int target){
+
+        return 0;
+        }
+};
+
+int main(){
+
+    /*
+    Solution s1;
+
+    vector <int> vec1 = {1,2,3,4,5};
+
+    int default = 1;
+
+    vector<int> v()
+
+    vector <vector <int>> matrix 
+
+    */
+
+
+return 0;
+}

2bd80a0cccbece7018ee46d8923b1b9aefadd850
Slav10 <59934391+Slav10@users.noreply.github.com>
2022-06-13 15:54:38 -0600

    Add files via upload

---
+++ 226.InvertBinaryTree/226.InvertBinaryTree.cpp
@@ -0,0 +1,9 @@
+TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr){
+            return nullptr;
+        }
+        swap(root->left, root->right);
+        invertTree(root->right);
+        invertTree(root->left);
+    return root;
+}

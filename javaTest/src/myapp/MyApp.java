36d014fd04bc91417ef7d4e9cfdcd41071ae934a
Slav10 <59934391+Slav10@users.noreply.github.com>
2022-06-16 20:25:31 -0600

    Add files via upload

---
+++ javaTest/src/myapp/MyApp.java
@@ -0,0 +1,11 @@
+package myapp;
+
+public class MyApp {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        for (String text : args){
+            System.out.println(text);
+        }
+    }
+}

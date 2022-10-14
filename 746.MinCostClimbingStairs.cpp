d0ff82580700ab15c4450784711a7c647071003f
Slav Ivanovich <59934391+Slav10@users.noreply.github.com>
2022-10-13 21:31:11 -0600

    Update 746.MinCostClimbingStairs.cpp

--- 746.MinCostClimbingStairs.cpp
+++ 746.MinCostClimbingStairs.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        if (cost.size() <= 2) return min(cost[cost.size() - 1], cost[cost.size() - 2]);
-        
-        for (int i = cost.size() - 3; i >= 0; i--) cost[i] = cost[i] + min(cost[i+1], cost[i+2]);
+    
+        for (int i = cost.size() - 3; i >= 0; i--)
+            cost[i] += min(cost[i+1], cost[i+2]);
         
     return min(cost[0], cost[1]);
     }

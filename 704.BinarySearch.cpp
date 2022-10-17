b1c5c526b64f215df020a432e24f3e4e2f1cf8b5
Slav Ivanovich <59934391+Slav10@users.noreply.github.com>
2022-10-16 22:15:14 -0600

    Create 704.BinarySearch.cpp

---
+++ 704.BinarySearch.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0; int high = nums.size()-1;
+        int mid = high/2;
+        
+        while (low <= high){
+            
+            if (nums[mid] == target){
+                return mid;
+            }
+            else if (nums[mid] < target){
+                low = mid + 1;
+                mid = (high+low)/2;
+            }
+            else if (nums[mid] > target){
+                high = mid - 1;
+                mid = high/2;
+            }
+        }
+        
+    return -1;
+    }
+};

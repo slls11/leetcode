737a87995ae0547832796e980d83833aa8daa1d2
Slav Ivanovich <59934391+slls11@users.noreply.github.com>
2022-10-23 23:45:39 -0600

    Update 438.FindAllAnagramsinaString.cpp
    Runtime: O(n)

--- 438.FindAllAnagramsinaString.cpp
+++ 438.FindAllAnagramsinaString.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
+        vector<int> ans;
+        if (p.length() > s.length()) return ans;
         
+        vector<int> vecS(26,0), vecP(26,0);
+        for (int i = 0; i < p.length(); i++){
+            vecS.at(s[i] - 'a')++;
+            vecP.at(p[i] - 'a')++;
+        }  
+        for (int i = p.length() - 1; i < s.length() - 1; i++){
+            if (vecS == vecP) ans.push_back(i - p.length() + 1);
+            vecS.at(s[i-p.length()+1] - 'a')--;
+            vecS.at(s[i+1] - 'a')++;
+        }
+        if (vecS == vecP) ans.push_back(s.length() - p.length());
+
+    return ans;
     }
 };

4724bec74109667b47d71bbb872d420d8fd52312
Slav Ivanovich <59934391+slls11@users.noreply.github.com>
2022-11-27 09:57:12 -0700

    Update 424.LongestRepeatingCharacterReplacement.cpp

--- 424.LongestRepeatingCharacterReplacement.cpp
+++ 424.LongestRepeatingCharacterReplacement.cpp
@@ -1,1 +1,26 @@
-
+/* brute force, O(n^2) */
+class Solution {
+public:
+    int max_value(map<char,int>& count){
+        int max = 0;
+        for (auto iter : count){
+            if (iter.second > max) max = iter.second;
+        }
+    return max;
+    }
+    
+    int characterReplacement(string s, int k) {
+        map<char,int> count;
+        int length = 0, max_length = 0, left = 0;
+        for (int right = 0; right < s.length(); right++){
+            count[s[right]]++; 
+            length++;
+            while (length - max_value(count) > k){
+                count[s[left]]--;
+                left++; length--;
+            }
+            if (length > max_length) max_length = length;
+        }
+    return max_length;
+    }
+};

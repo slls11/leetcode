812ba7a84216a86efd392401c7d8a0705328a4a7
Slav Ivanovich <59934391+slls11@users.noreply.github.com>
2023-02-01 11:10:41 -0700

    Update 572.SubtreeofAnotherTree.cpp

--- 572.SubtreeofAnotherTree.cpp
+++ 572.SubtreeofAnotherTree.cpp
@@ -1,34 +1,22 @@
-lass TimeMap {
-  /* Constant lookup, then log(n) for binary search, Runtime: O(log(n)) */
+
+class Solution {
 public:
-    void set(string key, string value, int timestamp) {
-        map[key].push_back({timestamp,value});
-    }
-    
-    string binary_search(vector<pair<int,string>>& vec, int timestamp){
-        if (!vec.size() || timestamp < vec[0].first) return "";
+    bool same = false;
+    bool match(TreeNode* root, TreeNode* subRoot){
+        if (!root && !subRoot) return true;
+        else if  (!root || !subRoot) return false;
+        else if (root->val != subRoot->val) return false;
 
-        int low = 0, high = vec.size()-1;
-        int mid = high/2;
-        while (low <= high){
-            if (vec[mid].first <= timestamp && (mid == vec.size()-1 || vec[mid+1].first > timestamp))
-                return vec[mid].second;
-            else if (vec[mid].first > timestamp){
-                high = mid-1;
-                mid = (high+low)/2;
-            }
-            else{
-                low = mid + 1;
-                mid = (high+low)/2;
-            }
-        }
-
-    return vec[mid].second;  
-    }
-    
-    string get(string key, int timestamp) {
-    return binary_search(map[key], timestamp);
+    return (match(root->left, subRoot->left) && match(root->right, subRoot->right));
     }
 
-    unordered_map<string,vector<pair<int,string>>> map;
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (root == nullptr) return false;
+        else if (match(root, subRoot)) same = true;
+        
+        if (!same) isSubtree(root->left, subRoot);
+        if (!same) isSubtree(root->right, subRoot);
+        
+    return same;
+    }
 };

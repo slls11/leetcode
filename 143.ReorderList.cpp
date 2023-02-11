babb4bffe6889f64a6b3fa17e271388b63c9541e
Slav Ivanovich <59934391+slls11@users.noreply.github.com>
2023-02-11 06:51:48 -0700

    Runtime: O(n), Space: O(1)

--- 143.ReorderList.cpp
+++ 143.ReorderList.cpp
@@ -1,4 +1,60 @@
 class Solution {
+/* Runtime: O(n), Space: O(1) */
+public:
+    ListNode* reverse(ListNode* back){
+        ListNode* middle = back->next;
+        ListNode* front = middle;
+
+        back->next = nullptr;
+        while (middle != nullptr){
+            front = front->next;
+            middle->next = back;
+            back = middle;
+            middle = front;
+        }
+    return back;
+    }
+
+    void reorderList(ListNode* head) {
+        if (!head->next || !head->next->next) return;
+
+        ListNode* middle = head;
+        ListNode* back = head;
+        while (back->next != nullptr){
+            if (back->next->next == nullptr){
+                middle = middle->next;
+                back = back->next; 
+            }
+            else{
+                middle = middle->next;
+                back = back->next->next;
+            }
+        }
+         
+        ListNode* right_start = reverse(middle->next);
+        middle->next = nullptr;
+
+        ListNode* left_start = head;
+        ListNode* new_right_start = right_start;
+
+        while (left_start->next != nullptr){
+            if (new_right_start == nullptr) break;
+            else new_right_start = new_right_start->next;
+
+            right_start->next = left_start->next;
+            left_start->next = right_start;
+
+            left_start = right_start->next;
+            right_start = new_right_start;
+        }
+
+    }
+};
+
+
+
+
+class Solution {
 /* Runtime: O(n^2) */
 public:
     void reorderList(ListNode* head) {

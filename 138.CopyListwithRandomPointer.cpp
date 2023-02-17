0c5718a3d58d48b68f4690db38792fa655b0e847
Slav Ivanovich <59934391+slls11@users.noreply.github.com>
2023-02-17 07:08:09 -0700

    Runtime: O(n)

--- 138.CopyListwithRandomPointer.cpp
+++ 138.CopyListwithRandomPointer.cpp
@@ -1,4 +1,45 @@
 class Solution {
+    /* Runtime: O(n) */
+public:
+    Node* copyRandomList(Node* head) {
+        unordered_map<Node*,short> map;
+        Node* temp_head = head;
+        short index = 0;
+        while (temp_head != nullptr){
+            map[temp_head] = index;
+            index++;
+            temp_head = temp_head->next; 
+        }
+        map[nullptr] = index;
+
+        vector<short> indices;
+        temp_head = head;
+        while (temp_head != nullptr){
+            indices.push_back(map[temp_head->random]);
+            temp_head = temp_head->next;
+        }
+
+        vector<Node*> node_address;
+        temp_head = head;
+        while (temp_head != nullptr){
+            Node* new_node = new Node(temp_head->val);
+            node_address.push_back(new_node);
+            temp_head = temp_head->next;
+        }
+        node_address.push_back(nullptr);
+
+        for (short i = 0; i < node_address.size()-1; i++){
+            node_address[i]->next = node_address[i+1];
+            node_address[i]->random = node_address[indices[i]];
+        }
+
+    return node_address[0];
+    }
+};
+
+
+
+class Solution {
   /* Runtime: O(n^2) */ 
 public:
     int find_index(Node* head, Node* target){

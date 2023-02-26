ad5df4c9766259a3fdabea5e39d15c342f0010e7
Slav Ivanovich <59934391+slls11@users.noreply.github.com>
2023-02-25 22:17:11 -0700

    Update 102.BinaryTreeLevelOrderTraversal.cpp

--- 102.BinaryTreeLevelOrderTraversal.cpp
+++ 102.BinaryTreeLevelOrderTraversal.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if (root == nullptr) return ans;
+
+        vector<TreeNode*> temp {root};
+        vector<TreeNode*> new_level;
+        while (1){
+            vector<int> curr_vec;
+
+            for (int i = 0; i < temp.size(); i++){
+                TreeNode* node = temp[i];
+                curr_vec.push_back(node->val);
+                if (node->left != nullptr) new_level.push_back(node->left);
+                if (node->right != nullptr) new_level.push_back(node->right);
+            }
+
+            ans.push_back(curr_vec);
+            temp = new_level;
+            new_level.clear();
+
+            if (temp.size() == 0) break;
+        }
+
+    return ans;
+    }
+};
+
+
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> output;
         
         if (root == nullptr){return output;}

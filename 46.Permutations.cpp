11570d6323891184eb343482bc1fa806831e5b0b
Slav Ivanovich <59934391+slls11@users.noreply.github.com>
2023-03-13 11:11:25 -0600

    Update 46.Permutations.cpp

--- 46.Permutations.cpp
+++ 46.Permutations.cpp
@@ -1,4 +1,28 @@
 class Solution {
+  /* Optimal i think... */
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        dfs(nums, ans, 0);
+    return ans;
+    }
+
+    void dfs(vector<int>& nums, vector<vector<int>>& ans, int start){
+        if (start == nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+
+        for (int i = start; i < nums.size(); i++){
+            swap(nums[start], nums[i]);
+            dfs(nums, ans, start+1);
+            swap(nums[start], nums[i]);
+        }
+    }
+};
+
+
+class Solution {
   /* Brute Force, not optimal */
 public:
     vector<vector<int>> permute(vector<int>& nums) {

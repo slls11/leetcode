011f723594b008c8e75b551350789d65c2c9c6c5
Slav Ivanovich <59934391+slls11@users.noreply.github.com>
2023-03-14 13:53:58 -0600

    Update 74.Searcha2DMatrix.cpp

--- 74.Searcha2DMatrix.cpp
+++ 74.Searcha2DMatrix.cpp
@@ -1,5 +1,37 @@
 class Solution {
 public:
+    bool binary_search(vector<int>& nums, int target){
+        int low = 0, high = nums.size()-1;
+        int mid = high/2;
+        while (low <= high){
+            if (nums[mid] == target) return true;
+            else if (nums[mid] < target){
+                low = mid + 1;
+                mid = (high+low)/2;
+            }
+            else if (nums[mid] > target){
+                high = mid - 1;
+                mid = high/2;
+            }
+        } 
+    return false;
+    }
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.size() == 1) return binary_search(matrix[0], target);
+
+        for (int i = 1; i < matrix.size(); i++){
+            if (target < matrix[i][0]) return binary_search(matrix[i-1], target);
+        }
+        return binary_search(matrix[matrix.size()-1], target);
+    return false;
+    }
+};
+
+
+
+class Solution {
+public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         for (int i = 0; i < matrix.size(); i++){
             if (target >= matrix[i][0] && (i == matrix.size()-1 || target < matrix[i+1][0])){

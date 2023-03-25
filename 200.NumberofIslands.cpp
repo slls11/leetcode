8faced579b61e8afc5162a2521d8c7065f6d273f
Slav Ivanovich <59934391+slls11@users.noreply.github.com>
2023-03-24 20:02:50 -0600

    Update 200.NumberofIslands.cpp

--- 200.NumberofIslands.cpp
+++ 200.NumberofIslands.cpp
@@ -1,5 +1,36 @@
 class Solution {
 public:
+    int numIslands(vector<vector<char>>& grid) {
+        int count = 0;
+        for (int i = 0; i < grid.size(); i++){
+            for (int j = 0; j < grid[0].size(); j++){
+                if (grid[i][j] == '1'){
+                    count++;
+                    search(grid, i, j);
+                }
+            }
+        }
+    return count;
+    }
+
+    void search(vector<vector<char>>& grid, int i, int j){
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == '0') return;
+        
+        grid[i][j] = '0';
+
+        search(grid, i+1, j);
+        search(grid, i-1, j);
+        search(grid, i, j+1);
+        search(grid, i, j-1);
+    }
+};
+
+
+
+
+
+class Solution {
+public:
     int count = 0;
     
     void fullIsland(vector<vector<char>> &grid, int x, int y){  

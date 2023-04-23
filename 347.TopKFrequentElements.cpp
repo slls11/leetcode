bdd56be38c2c87030286840b67fa6de5585f7e4b
Slav Ivanovich <59934391+slls11@users.noreply.github.com>
2023-04-23 10:10:15 -0600

    Runtime: O(n), Space: O(n)

--- 347.TopKFrequentElements.cpp
+++ 347.TopKFrequentElements.cpp
@@ -1,4 +1,30 @@
 class Solution {
+  /* Runtime: O(n), Space: O(n) */
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> map;
+        for (auto& it : nums) map[it]++;
+
+        vector<vector<int>> count(nums.size()+1);
+        for (auto& it : map) count[it.second].push_back(it.first);
+
+        vector<int> ans;
+        for (int i = count.size()-1; k > 0 && i > 0; i--){
+            if (count[i].size() > 0){
+                for (int j = 0; j < count[i].size(); j++){
+                    ans.push_back(count[i][j]);
+                    k--;
+                }
+            }
+        }
+
+    return ans;
+    }
+};          
+
+
+
+class Solution {
   /* O(nlog(n)) solution? */
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {

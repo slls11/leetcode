36672ca99e92942fd6f94b59dc708f838fcda0ad
Slav Ivanovich <59934391+slls11@users.noreply.github.com>
2023-04-29 19:00:20 -0600

    Runtime: O(n^2), Space: O(1)

--- 5.LongestPalindromicSubstring.cpp
+++ 5.LongestPalindromicSubstring.cpp
@@ -1,4 +1,29 @@
 class Solution {
+  /* Runtime: O(n^2), Space: O(1) */
+public:
+    string longestPalindrome(string s) {
+        string max_str(1, s[0]);
+        for (int i = 0; i < s.size(); i++){
+            expand(s, i, i, max_str);
+            expand(s, i, i+1, max_str);
+        }
+    return max_str;
+    }
+
+    void expand(const string& s, int i, int j, string& max_str){
+        while(i > -1 && j < s.size()){
+            if (s[i] != s[j]) break;
+            i--; j++;
+        }
+        i++; j--;
+
+        if (j-i+1 > max_str.size()) max_str = s.substr(i, j-i+1);
+    }
+};
+
+
+
+class Solution {
   /* Brute force: O(n^3) */   
 public:
     string longestPalindrome(string s) {

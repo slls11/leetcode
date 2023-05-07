eee629af1e37d400a7efcc098185109358169cdb
Slav Ivanovich <59934391+slls11@users.noreply.github.com>
2023-05-06 23:35:37 -0600

    Update 322.CoinChange.cpp

--- 322.CoinChange.cpp
+++ 322.CoinChange.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> count(amount+1, amount+1);
+        count[0] = 0;
 
+        for (int num = 1; num <= amount; num++){
+            for (int denom = 0; denom < coins.size(); denom++){
+                if (num - coins[denom] < 0) continue;
+                count[num] = min(count[num], 1 + count[num - coins[denom]]);
+            }
+        }
+
+        if (count[amount] == amount+1) return -1;
+    
+    return count[amount];
+    }
+};

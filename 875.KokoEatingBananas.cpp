566c41660a8224ce15d0197dfcab37cada6b0f4b
Slav Ivanovich <59934391+slls11@users.noreply.github.com>
2023-05-18 11:21:03 -0600

    Update 875.KokoEatingBananas.cpp

--- 875.KokoEatingBananas.cpp
+++ 875.KokoEatingBananas.cpp
@@ -1,5 +1,46 @@
 class Solution {
 public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1, high = *max_element(piles.begin(), piles.end());
+        if (h == piles.size()) return high;
+
+        int mid = high/2;
+        while(low < high){
+            int smaller_rate_time = count_time(piles, mid-1);
+            int mid_rate_time = count_time(piles, mid);
+
+            if (smaller_rate_time > h && mid_rate_time <= h) return mid;
+            else if (smaller_rate_time > h && mid_rate_time > h){
+                low = mid+1;
+                mid = (high+low)/2;
+            }
+            else{
+                high = mid-1;
+                mid = (high+low)/2;
+            }
+        }
+        
+    return mid;
+    }
+
+    int count_time(const vector<int>& piles, int k){
+        int time = 0;
+        for (int it : piles){
+            if (it % k == 0) time += it/k;
+            else time += (it/k) + 1;
+        }
+    return time;
+    }
+};
+
+
+
+
+
+
+
+class Solution {
+public:
     int count_time(int k, vector<int>& piles){
         int i = 0;
         unsigned int total = 0;
